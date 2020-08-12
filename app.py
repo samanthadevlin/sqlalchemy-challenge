32d2e793a566564c235f44ef78cb681630ab303a
samanthadevlin <64792728+samanthadevlin@users.noreply.github.com>
2020-08-12 19:19:57 -0400

    push

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import session
+from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect 
 from flask import Flask, jsonify
 
@@ -11,7 +11,7 @@ engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 #reflect an existing database into a new model 
 Base = automap_base()
 #reflect tables
-Base.prepare(engine, refect = True)
+Base.prepare(engine, reflect = True)
 #save reference to the table 
 Measurement = Base.classes.measurement
 Station = Base.classes.station
@@ -20,72 +20,100 @@ Station = Base.classes.station
 app = Flask(__name__)
 #flask routes
 @app.route("/")
-def welcome()
-"""List all available apt routes."""
-return(
-    f"Available Routes:<br/>"
-    f"/api/v1.0/precipitation"
-    f"/api/v1.0/stations"
-    f"/api/v1.0/tobs"
-)
+def welcome():
+#"""List all available apt routes."""
+    return(
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start_date><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
 
 @app.route("/api/v1.0/precipitation")
-def precipitation()
+def precipitation():
 #create our session link from python to the DB 
-session = Session(engine)
-"""Return a list of precipitation"""
-results = session.query(Measurement.date, Measurement.prcp).all()
-session.close()
+    session = Session(engine)
+##"""Return a list of precipitation"""
+
+    results = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date > '2016-08-22').order_by(Measurement.date).all()
+
+   
 #create a dictionary from the row data and append to list of date and prcp 
-measure_date = []
-for date, prcp in results:
-    preci_dict = {}
-    preci_dict = ["date"] = date
-    preci_dict = ['prcp'] = precipitation
-    measure_date.append(preci_dict)
-return jsonify(measure_date)
+    measure_date = []
+    for i in results:
+        preci_dict = {}
+        preci_dict[i[0]] = i[1]
+        #preci_dict = ['prcp'] = precipitation
+        measure_date.append(preci_dict)
+
+    return jsonify(measure_date)
 
 @app.route("/api/v1.0/stations")
-def stations()
+def stations():
 #create our session link from python to the DB 
-session = Session(engine)
-"""Return a list of station"""
-results = session.query(Station.station, Station.name).all()
-session.close()
+    session = Session(engine)
+#"""Return a list of station"""
+    results = session.query(Station.station, Station.name).all()
+    session.close()
+
+    return jsonify(results)
 
 @app.route("/api/v1.0/tobs")
-def tobs()
+def tobs():
 #create our session link from python to the DB 
-session = Session(engine)
-"""Return a list of station"""
-results = session.query(Measurement.date, Measurement.tobs, Measurement.station).filter(Measurement.date > '2016-08-22').all()
-session.close()
-tobs_obv = []
-for temperature observation, date, station in results:
-    tobs_temp_dict = {}
-    tobs_temp_dict["date"] = date
-    tobs_temp_dict["tobs"] = temperature_observation
-    tobs_temp_dict["station"] = station
-tobs_obv.append(tobs_temp_dict)
-return jsonify(tobs_obv)
+    session = Session(engine)
+#"""Return a list of station"""
+    results = session.query(Measurement.date, Measurement.tobs, Measurement.station).filter(Measurement.date > '2016-08-22').all()
+    session.close()
+    tobs_obv = []
+    for temperature_observation, date, station in results:
+        tobs_temp_dict = {}
+        tobs_temp_dict["date"] = date
+        tobs_temp_dict["tobs"] = temperature_observation
+        tobs_temp_dict["station"] = station
+        tobs_obv.append(tobs_temp_dict)
+    return jsonify(tobs_obv)
 
-@app.route("/api/v1.0/<start>")
-def start_date()
+@app.route("/api/v1.0/<start_date>")
+def single_date(start_date):
 #create our session link from python to the DB 
-session = Session(engine)
-"""Return a list of station""" 
-results = session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs).all()
-session.close()
-return jsonify(start_date)
+    session = Session(engine)
+#"""Return a list of station""" 
+    results = session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
+    filter(Measurement.date >= start_date).all()
+    
+    single_date = []
+    for x in results:
+        start_date_dict = {}
+        start_date_dict["Min Temp"] = results[0][0]
+        start_date_dict["Avg Temp"] = results[0][1]
+        start_date_dict["Max Temp"] = results[0][2]
+        single_date.append(start_date_dict)
+
+    #session.close()
+    return jsonify(single_date)
 
 @app.route("/api/v1.0/<start>/<end>")
-def end_date()
+def range_date(start, end):
 #create our session link from python to the DB 
-session = Session(engine)
-"""Return a list of station""" 
-results = session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs).all()
-session.close()
-return jsonify(end_date)
+    session = Session(engine)
+#"""Return a list of station""" 
+    results = session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
+    filter(Measurement.date >= start).filter(Measurement.date <= end).all()
 
-if __name__ == '__main__'
-app.run(debug=True)
+    range_date = []
+    for x in results:
+        range_date_dict = {}
+        range_date_dict["Min Temp"] = results[0][0]
+        range_date_dict["Avg Temp"] = results[0][1]
+        range_date_dict["Max Temp"] = results[0][2]
+        range_date.append(range_date_dict)
+
+    session.close()
+    return jsonify(range_date)
+
+if __name__ == '__main__':
+    app.run(debug=True)
